3fdcedaaad411e47df0bb7d4aab64fb57b9bd849
Garret Adkins <garret5288@gmail.com>
2025-08-22 11:18:45 -0400

    made my art gallery a
    separated my art into better file structure as well as making new cat art

---
+++ week1/projects/asciiartgallery/welcome.py
@@ -0,0 +1,11 @@
+line01 = '********************'
+line02 = '*                  *'
+line03 = '*     Welcome!     *'
+# Print a welcome box using ASCII characters
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+print('')
