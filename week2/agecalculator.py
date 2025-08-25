01f65719c6093e17ea35d1b9c6fbec4f2a9cbd3d
Garret Adkins <garret5288@gmail.com>
2025-08-25 10:16:34 -0400

    made some simple input programs
    made three simple input programs and changed some file names

---
+++ week2/agecalculator.py
@@ -0,0 +1,4 @@
+Birth_year = input("What year were you born? ")
+current_year = 2025
+age = current_year - int(Birth_year)
+print("You are " + str(age) + " years old.")
---
+++ week2/colorcollector.py
@@ -0,0 +1,11 @@
+colors = []
+
+while True:
+    color = input("Enter a favorite color (or type done to finish): ")
+    if color.lower() == "done":
+        break
+    colors.append(color)
+
+print("Your favorite colors are:")
+for c in colors:
+    print("- " + c)
