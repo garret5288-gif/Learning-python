e8f3d76f9c615153987aefcf595fb12353e8c478
Garret Adkins <garret5288@gmail.com>
2025-08-26 06:44:17 -0400

    testing types
    messed around with defining types then converting types

---
+++ week2/typeconv.py
@@ -0,0 +1,11 @@
+num_int = 10
+print(type(num_int))
+
+num_str = "7"
+print(type(num_str))
+
+num_float = 7.34
+print(type(num_float))
+
+num_sum = num_int + int(num_str) + num_float
+print(num_sum)
---
+++ week2/types.py
@@ -0,0 +1,11 @@
+int = 8
+print(int)
+print(type(int))
+
+str = "5"
+print(str)
+print(type(str))
+
+float = 9.32
+print(float)    
+print(type(float))
