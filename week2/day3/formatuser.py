bff07772b4bb67a6d8ace3e3d986fe2663b13451
Garret Adkins <garret5288@gmail.com>
2025-08-27 09:32:09 -0400

    made folders and a name formatter

---
+++ week2/day3/formatuser.py
@@ -0,0 +1,4 @@
+user_name = input("Enter your username: ").strip().upper()
+city = input("Enter your city: ").strip().capitalize()
+school = input("Enter the school you graduated from: ").strip().title()
+[print(f"Hello, {user_name} from {city}, graduate of {school}!")]
