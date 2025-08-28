2a1871648c8c4810c3e6addf0b44fa8c89ea0d46
Garret Adkins <garret5288@gmail.com>
2025-08-28 17:50:40 -0400

    added comments

--- week2/day4/checkinput.py
+++ week2/day4/checkinput.py
@@ -1,11 +1,12 @@
+# Check for valid username
 user_name = input("Enter your username that doesn't contain numbers: ").capitalize()
 
-while not user_name.isalpha():
+while not user_name.isalpha():# Check if the username contains only letters
     print("Invalid username. Please use only letters.")
 
 age = input("Enter your age: ")
 
-while not age.isdigit():
+while not age.isdigit():# Check if the age is a number
     print("Invalid age. Please enter a number.")
 print(f"Hello, {user_name} your age is {age}!")
 
