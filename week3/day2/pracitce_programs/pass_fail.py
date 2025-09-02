ae660ad142179be95b638607a701aa5080312f63
Garret Adkins <garret5288@gmail.com>
2025-09-02 08:43:53 -0400

    moved files and practiced if else statements

---
+++ week3/day2/pracitce_programs/pass_fail.py
@@ -0,0 +1,6 @@
+grade = int(input("Enter your grade: "))
+
+if grade >= 60:
+    print("You passed!")
+else:
+    print("You failed.")
---
+++ week3/day2/simple_bot.py
@@ -0,0 +1,9 @@
+temp = input("Enter the temperature in Fahrenheit: ")
+if temp > 85:
+    print("It's hot outside!")
+    print("Wear light clothing.")
+elif temp < 60:
+    print("It's cold outside!")
+    print("Wear warm clothing.")
+else:
+    print("The weather is mild.")
