ffcb38061e1c89319badf1e115a013672a0eacdb
Garret Adkins <garret5288@gmail.com>
2025-09-08 08:42:55 -0400

    messing around with the loops and breaking them

--- week4/day1/practice/num_pattern_printer.py
+++ week4/day1/practice/num_pattern_printer.py
@@ -1,15 +1,14 @@
+# num_pattern_printer.py
 
-
-while True:
-    num = input("Enter a number: ").strip()
-    
+while True: # Loop until valid input is received
+    num = input("Enter a number: ").strip()   
     if num == "":
         print("Enter a number")
     elif not num.isdigit():
         print("Enter a valid number")
     elif int(num) <= 0:
         print("Enter a number greater than 0")
-    else:
-      for i in range(1, int(num) + 1, 2):
+    else: # Valid input received
+        for i in range(1, int(num) + 1, 2): # Print odd numbers from 1 to num
             print(i)
-    break
+        break # Exit the loop after printing
