ebea37dd676e3a4c615b6eeca72ff951e25c27f6
Garret Adkins <garret5288@gmail.com>
2025-09-19 09:23:31 -0400

    changed some files

--- week5/day5/cs50_ceasar.py
+++ week5/day5/cs50_ceasar.py
@@ -9,6 +9,7 @@ def main(): #main function to run the program
     print("*************")
     action = input("Do you want to encrypt or decrypt a message? (Press E or D): ")
     print()
+    
     if action.lower() == "e": # encrypt
         text = input("Enter your message to encrypt: ")
         key = int(input("Enter the key (number of positions to shift): "))
@@ -22,7 +23,22 @@ def main(): #main function to run the program
     else:
         print("Invalid action. Please choose either 'E' for encrypt or 'D' for decrypt.")
 
-def encrypt_decrypt(text, mode, key): # function to encrypt or decrypt the message
+def encrypt_decrypt(text, mode, key)->str | int: # function to encrypt or decrypt the message
+    """
+    Encrypts or decrypts a message using the Caesar cipher.
+
+    Args:
+        text (str): The input text to encrypt or decrypt.
+        mode (str): "e" for encryption, "d" for decryption.
+        key (int): The number of positions to shift the letters.
+
+    Returns:
+        str: The encrypted or decrypted message.
+    
+
+
+    Returns:_type_: _description_
+    """
     result = "" # initialize result string
     if  mode == "d": 
         key = -key # reverse the key for decryption
